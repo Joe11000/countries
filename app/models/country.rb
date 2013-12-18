85dae02a2af889efa6ea0b951071bae54e8d717d
Joe Noonan <joenoonan27@gmail.com>
2013-12-18 13:00:16 -0600

    formatting

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,21 +1,21 @@
 class Country < ActiveRecord::Base
-	attr_accessible :name, :location
+  attr_accessible :name, :location
 
-	belongs_to  :ruler, class_name: "User", 
-	                    inverse_of: :country
+  belongs_to  :ruler, class_name: "User", 
+	                  inverse_of: :country
 
-	has_many  :sent_messages, dependent:   :destroy,
+  has_many    :sent_messages, dependent:   :destroy,
                               class_name:  "Message",
 	                          foreign_key: :sent,
 	                          inverse_of:  :country
 
-	has_many  :received_messages, dependent:   :destroy,
+  has_many    :received_messages, dependent:   :destroy,
 	                              class_name:  "Message",
 	                              foreign_key: :received,
 	                              inverse_of:  :country
 
-	validates :name, presence:   true, 
+  validates   :name, presence:   true, 
 	                 uniqueness: true
 
-	validates :location, presence: true
+  validates   :location, presence: true
 end
--- db/migrate/20131217234205_create_users.rb
+++ db/migrate/20131217234205_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name_first
-	    t.string :name_last
-	    t.string :password_digest
+	  t.string :name_last
+	  t.string :password_digest
 
       t.timestamps
     end
