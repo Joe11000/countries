a2ff5d037be31d91df7386cc2b78d7c6ea5f0873
Joe Noonan <joenoonan27@gmail.com>
2013-12-19 13:14:24 -0600

    message_spec.rb basics running

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Message do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  	let(:message){ FactoryGirl.create(:message)}
+  	let(:country1) {FactoryGirl.build_stubbed(:country)}
+  	let(:country2) {FactoryGirl.build_stubbed(:country)}
+  	
+  describe "factory" do
+  	it "should be valid" do
+  		expect(message.subject).to eq "sub"
+  		expect(message.body).to eq "body"
+  	end
+  end
+
+  describe "model" do
+  	it {should allow_mass_assignment_of(:body)}
+  	it {should allow_mass_assignment_of(:subject)}
+
+  	it { should validate_presence_of(:body) }
+  	it { should validate_presence_of(:subject) }
+
+  	it { should belong_to(:sent).class_name(:Country)}
+  	it { should belong_to(:received).class_name(:Country)}
+  end
 end
