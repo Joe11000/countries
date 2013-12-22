7ede35e09c73ab63be3924732bc8415202556a89
Joe Noonan <joenoonan27@gmail.com>
2013-12-22 16:38:37 -0600

    more sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,18 @@
 class SessionsController < ApplicationController  
   def new # log in
-    if current_user
-      redirect_to @current_user
+    if not params[:id] 
+    	redirect_to @root
+    end
+    
+    if not current_user
+      @current_user = User.find_by_id(params[:id]) 
+      if @current_user.valid?
+        session[:id] = @current_user
+      end
     end
 
-    @current_user = User.find_by_id(params[:id]) 
-    if @current_user.valid?
-      session[:id] = @current_user
-    end
-
-    redirect_to 'user#show'
+    redirect_to @current_user
   end
-
 
   def delete
     session[:id] = nil
